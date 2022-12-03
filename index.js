706f5c8a0378e374a71684f39ff96b160a1df973
Brian <wamwangimathenge@gmail.com>
2022-12-03 17:52:14 +0300

    Query Selector Methods Added

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log("hi")
+
+
+
+const li2 = document.querySelector("ul.ranked-list li ul li")
+console.log(li2);
+
+
+const div4 = document.querySelector("ul.unranked-list li div")
+console.log(div4)
